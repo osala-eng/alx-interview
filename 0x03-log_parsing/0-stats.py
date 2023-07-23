98d855d89e8797a6d2f1b8609d8fce2c6594149f
Jashon Osala <jashonosala@gmail.com>
2023-07-23 11:42:18 +0300

    Format

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,36 +4,31 @@ from contextlib import suppress
 from sys import stdin
 
 
-StatusCodes = dict[int, int]
-FileSize = list[int]
+if __name__ == '__main__':
+    StatusCodes = dict[int, int]
+    FileSize = list[int]
 
+    def _print_stats(file_size: FileSize, status_codes: StatusCodes) -> None:
+        """Private method to print stats."""
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if key in status_codes:
+                print('{}: {}'.format(key, status_codes[key]))
 
-def _print_stats(file_size: FileSize, status_codes: StatusCodes) -> None:
-    """Private method to print stats."""
-    print('File size: {}'.format(file_size[0]))
-    for key in sorted(status_codes.keys()):
-        if key in status_codes:
-            print('{}: {}'.format(key, status_codes[key]))
-
-
-def _parse_line(
-    line: str,
-    status_codes: StatusCodes,
-    file_size: FileSize
-        ) -> tuple[StatusCodes, FileSize]:
-    """A private method to parse lines."""
-    with suppress(BaseException):
-        line = line[:-1]
-        word = line.split(' ')
-        file_size[0] += int(word[-1])
-        status_code = int(word[-2])
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-    return status_codes, file_size
-
-
-def main() -> None:
-    """Main function to handle module logic."""
+    def _parse_line(
+        line: str,
+        status_codes: StatusCodes,
+        file_size: FileSize
+            ) -> tuple[StatusCodes, FileSize]:
+        """A private method to parse lines."""
+        with suppress(BaseException):
+            line = line[:-1]
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        return status_codes, file_size
 
     file_size = [0]
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
@@ -51,7 +46,3 @@ def main() -> None:
         _print_stats(file_size, status_codes)
         raise
     _print_stats(file_size, status_codes)
-
-
-if __name__ == '__main__':
-    main()
